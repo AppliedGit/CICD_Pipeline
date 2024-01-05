c6900ffcae34484c5acec25cfff8ff2fe32cc8f5
AppliedGit <62598762+AppliedGit@users.noreply.github.com>
2024-01-05 14:17:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+    
+def add(a,b):
+  print("ADD value")
+  return (a+b)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
