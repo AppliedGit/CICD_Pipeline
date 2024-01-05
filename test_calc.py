8f8921c29ccc9d7a6325fbf86399f6ac599085e3
AppliedGit <62598762+AppliedGit@users.noreply.github.com>
2024-01-05 14:18:07 +0530

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,9 +1,9 @@
 import unittest
-import calc
+import app
 
 class TestCalc(unittest.TestCase):
   def test_add(self):
-    self.assertEqual(calc.add(10,5),15)
+    self.assertEqual(app.add(10,5),15)
 
 if __name__ == "__main__":
   unittest.main()
